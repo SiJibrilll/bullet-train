80e9a7ce42b62ec7f46c93230ac4f695741c1422
SiJibrilll <jibrilianwa@gmail.com>
2025-05-30 20:37:31 +0700

    added collision system files

---
+++ core/src/main/java/studio/jawa/bullettrain/systems/technicals/MovementCollisionSystem.java
@@ -0,0 +1,13 @@
+package studio.jawa.bullettrain.systems.technicals;
+
+import com.badlogic.ashley.core.*;
+import com.badlogic.ashley.utils.ImmutableArray;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import studio.jawa.bullettrain.components.technicals.TransformComponent;
+import studio.jawa.bullettrain.components.technicals.VelocityComponent;
+
+
+public class MovementCollisionSystem extends EntitySystem {
+
+}
