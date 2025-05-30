621274cfffe3deba2f87ea528235adb1e0b3030b
SiJibrilll <jibrilianwa@gmail.com>
2025-05-30 07:59:10 +0700

    initial rendering feature set up

---
+++ core/src/main/java/studio/jawa/bullettrain/components/technicals/TransformComponent.java
@@ -0,0 +1,10 @@
+package studio.jawa.bullettrain.components.technicals;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.math.Vector2;
+
+public class TransformComponent implements Component {
+    public final Vector2 position = new Vector2();
+    public float rotation = 0f;
+    public float scale = 1f;
+}
